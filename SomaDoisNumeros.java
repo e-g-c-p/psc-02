44f25a2ff3a9ed387a468ef2549aef6fdcdca2d5
Eduardo Peres <edu@gcperes.com>
2023-09-04 21:40:37 -0300

    ap02(input_output): interface gráfica adicionada

--- SomaDoisNumeros.java
+++ SomaDoisNumeros.java
@@ -1,19 +1,16 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 public class SomaDoisNumeros {
 	public static void main(String[] args) {
-		Scanner leitor = new Scanner(System.in);
 		double primeiroOperando;
 		double segundoOperando;
 		double resultado;
 		
-		System.out.println("Digite o primeiro operando:");
-		primeiroOperando = leitor.nextDouble();
-		System.out.println("Digite o segundo operando:");
-		segundoOperando = leitor.nextDouble();
+		primeiroOperando = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro operando"));
+		segundoOperando = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo operando"));
 		
 		resultado = primeiroOperando + segundoOperando;
 
-		System.out.print("Resultado: ");
-		System.out.println(resultado);
+		JOptionPane.showMessageDialog(null, resultado);
 	}
 }
